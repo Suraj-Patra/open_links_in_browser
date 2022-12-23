5a0d52e6637d61ea52379e5f9bab0e0cd44063c0
Suraj Patra <patrasuraj2000@gmail.com>
2022-12-23 22:43:42 +0530

    Same as the previous, but options are added for better interface

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const readline = require('readline').createInterface({
     output: process.stdout
 })
 
-let [ method, browser ] = process.argv.slice(2);
+let [ browser ] = process.argv.slice(2);
 
 const gettingInputs = () => {
+    readline.question('a. File\nb.Links\nEnter option : ', option => {
 
-    let path = '';
-    if(method === 'file'){
-        readline.question('Enter file name : ', file => {
-            path = `./Files/${file}`;
-            readline.close();
+        let path = '';
+        if(option === 'a'){
+            readline.question('Enter file name : ', file => {
+                path = `./Files/${file}`;
+                readline.close();
+                openFile(path);
+            })
+        } else {
+            path = './links.txt';
             openFile(path);
-        })
-    } else {
-        if(method==='chrome' || method==='librewolf'){
-            browser = method;
         }
-        path = './links.txt';
-        openFile(path);
-    }
+    })
+
 }
 
 
@@ -79,15 +79,28 @@ gettingInputs();
 
 Process :
 
+    Command :
+
+        a. node script.js chrome    |--> For chrome
+        b. node script.js librewolf |--> For librewolf in private window
+
+        Type : a- for file option, b- for links option
+
+
+    From links :
     1. Install Node
     2. Create a directory --> save this file as 'script.js' inside it
     3. Create another file inside the directory called --> 'links.txt'
     4. Copy the links you want to open, in the 'links.txt'
     5. Give following command :
-
-        a. node script.js chrome    |--> For chrome
-        b. node script.js librewolf |--> For librewolf in private window
     
+
+    From file :
+    1. same
+    2. same
+    3. create another directory called 'Files'
+    4. Inside this directory copy the files with links you want to open.
+    5. In the file name give the name of the file.
 */
 
 
